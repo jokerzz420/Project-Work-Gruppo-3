71bea816a16932febc6507abec7f62d31e8d2a8e
PtrIvan <ivan.petrarolo@fitstic-edu.com>
2024-07-01 11:40:48 +0200

    Add files via upload

---
+++ report skipfish/child_index.js
@@ -0,0 +1,3 @@
+var child = [
+  { 'dupe': false, 'type': 2, 'name': 'http://192.168.109.1:3000/', 'dir': 'c0', 'linked': 2, 'url': 'http://192.168.109.1:3000/', 'fetched': true, 'code': 200, 'len': 3748, 'decl_mime': 'text/html', 'sniff_mime': 'application/xhtml+xml', 'cset': 'UTF-8', 'missing': false, 'csens': true, 'child_cnt': 16, 'issue_cnt': [ 12, 3, 13, 6, 0 ], 'sig': 0x8417ccae }
+];
---
+++ report skipfish/summary.js
@@ -0,0 +1,4 @@
+var sf_version = '2.10b';
+var scan_date  = 'Thu Jun 27 06:26:03 2024';
+var scan_seed  = '0x3de4a979';
+var scan_ms    = 76981;
